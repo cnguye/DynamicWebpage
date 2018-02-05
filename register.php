6d79f18ff450197e52e5ce59538038768556b227
Christopher Nguyen <ngyn.christopher@gmail.com>
2018-02-04 20:00:52 -0800

    foundation of website

---
+++ register.php
@@ -0,0 +1,42 @@
+<?php
+$title = "Home";
+$content = '<img src="Images/coffee1.png" class="imgLeft" />
+
+<h3> Register </h3>
+
+<p>
+    Lorem ipsum dolor sit amet, feugait theophrastus ex per. Ea 
+    insolens indoctum vituperatoribus per. Tantas consequat id est, 
+    sea placerat pertinax erroribus id. Ei quando tantas molestiae 
+    mea, cum ne omnis officiis consequat.
+    
+<p>
+    Rebum summo at nec, vis ridens perpetua cu, sit integre mentitum 
+    suscipiantur te. Id usu salutandi instructior conclusionemque, ne 
+    dolore latine ius, graeco doctus est te. Ut vis scribentur 
+    instructior, nam ad euismod elaboraret accommodare, ea sumo eius 
+    constituam eam. Id duo aliquid albucius reformidans. Quo graeci 
+    fabulas legimus cu, ferri possit cotidieque ut per, ea consequat 
+    eloquentiam cum. His delicata consectetuer ea, ut mea iriure aperiri 
+    disputationi, ad sit antiopam adolescens. No accumsan voluptatibus quo.
+    </p>
+
+</p>
+<img src="Images/coffee3.png" class="imgRight" />
+
+<h3> Lorem Ipsum </h3>
+
+<p>
+    Mei ut civibus explicari, vis dicta facete te, eos brute novum te. 
+    Ea vel incorrupte disputationi. His habeo illud simul ea, ancillae 
+    menandri no mea, graecis probatus vel at. Cum malis augue tamquam 
+    at. Ad sensibus dissentiunt vix, vel iuvaret conceptam ut, falli 
+    nulla oblique cu eum. Eros sale cum te, et ius inimicus expetenda 
+    dissentiunt.
+</p>
+
+';
+
+include "Template.php";
+?>
+
